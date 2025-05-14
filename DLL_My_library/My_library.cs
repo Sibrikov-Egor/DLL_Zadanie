6c01a5fcc0b3755f411edf66c07dbb9a33c81b1e
unknown <@ngknn.local>
2025-05-14 12:34:28 +0300

    first

---
+++ DLL_My_library/My_library.cs
@@ -0,0 +1,7 @@
+﻿namespace DLL_My_library
+{
+    public class My_library
+    {
+        public string ReplaceText(string oldValue, string newValue, string input) => input.Replace(oldValue, newValue);  
+    }
+}
